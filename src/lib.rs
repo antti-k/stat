acac57c400193df1cb12e706e14a73d4b8e6f20b
Antti <karvanen.antti@gmail.com>
2018-09-09 19:05:33 +0300

    Added mean and sum

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
 #[cfg(test)]
 mod tests {
+    use mean;
+    use sum;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_mean() {
+        assert_eq!(mean(&vec![1.0, 2.0, 3.0]), 2.0);
     }
+    #[test]
+    fn test_sum() {
+        assert_eq!(sum(&vec![1.0, 2.0, 3.0]), 6.0);
+    }
+}
+
+
+pub fn mean(values: &Vec<f64>) -> f64 {
+    sum(values) / (values.len() as f64)
+}
+
+pub fn sum(values: &Vec<f64>) -> f64 {
+    values.iter().fold(0.0, |a, b| a + b)
 }
