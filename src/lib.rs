cc02684f5affef8f60a9dc5dfea55d012c1b6ab1
Antti <karvanen.antti@gmail.com>
2018-09-30 14:22:59 +0300

    Added stdev function

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 mod tests {
     use mean;
     use sum;
+    use variance;
     #[test]
     fn test_mean() {
         assert_eq!(mean(&vec![1.0, 2.0, 3.0]), 2.0);
@@ -10,6 +11,10 @@ mod tests {
     fn test_sum() {
         assert_eq!(sum(&vec![1.0, 2.0, 3.0]), 6.0);
     }
+    #[test]
+    fn test_variance() {
+        assert_eq!(variance(&vec![0.0, 1.0]), 0.25);
+    }
 }
 
 
@@ -17,6 +22,19 @@ pub fn mean(values: &Vec<f64>) -> f64 {
     sum(values) / (values.len() as f64)
 }
 
-pub fn sum(values: &Vec<f64>) -> f64 {
+fn sum(values: &Vec<f64>) -> f64 {
     values.iter().fold(0.0, |a, b| a + b)
 }
+
+pub fn variance(values: &Vec<f64>) -> f64 {
+    let mean = mean(values);
+    values
+        .iter()
+        .map(|x| (x - mean).powf(2.0))
+        .fold(0.0, |a, b| a + b) / (values.len() as f64)
+}
+
+pub fn stdev(values: &Vec<f64>) -> f64 {
+    variance(values).powf(0.5)
+}
+    
